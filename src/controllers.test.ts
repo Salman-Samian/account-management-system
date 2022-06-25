878e212ffbdce486237a840a20deafe51e067cda
Salman Samian <salman.samian@gmail.comgit config --global user.email salman.samian@gmail.comgit config --global user.email salman.samian@gmail.com>
2022-06-25 11:24:21 +0300

    update v2.2 + test & fix bugs

--- src/controllers.test.ts
+++ src/controllers.test.ts
@@ -11,6 +11,8 @@ import {
 enableFetchMocks();
 
 describe('testing controller file', () => {
+
+    // initial data
     const totalDeposit: number = 5;
     const totalWithdraw: number = 100;
     const person: Person = {
@@ -34,6 +36,11 @@ describe('testing controller file', () => {
         createDate: new Date()
     };
 
+
+    test('add new account', () => {        
+        expect(addAccount(account)).resolves.toEqual(account);
+    });
+
     test('make deposit to a person account', () => {
         const ok_result: StatusMessage = {
             message: `${totalDeposit} value deposit successfully`,
@@ -42,6 +49,7 @@ describe('testing controller file', () => {
         };
         expect(depositToAccount(account, totalDeposit)).resolves.toEqual(ok_result);
     });
+
     test('withdrawal Operation', () => {
         expect(withdrawalOperation(account, totalWithdraw)).resolves.toEqual(notok_result);
     });
@@ -49,11 +57,10 @@ describe('testing controller file', () => {
     test('make balance Inquiry By Account', () => {
         expect(balanceInquiryByAccount(account)).resolves.toEqual(notok_result);
     });
+
     test('retrieves account Statement', () => {
         expect(retrievesTheAccountStatementOfTransactions(account)).resolves.toEqual(notok_result);
     });
-
-
 
     // if you run this test the user will be block and you should manually retrive in using the json file.
     // test('perform Block the account', () => {
