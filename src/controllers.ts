ea7b0329c1c3ddc0122076ef65a557eb29f019a5
Salman Samian <salman.samian@gmail.comgit config --global user.email salman.samian@gmail.comgit config --global user.email salman.samian@gmail.com>
2022-06-25 09:21:01 +0300

    A+ balance Inquiry By Account

--- src/controllers.ts
+++ src/controllers.ts
@@ -102,53 +102,11 @@ export async function balanceInquiryByAccount(account: Account, totalDeposit: nu
         type: false
     };
     const listAccounts = await readFileDataSource(filePathListAccounts);
-    const accountIsExists = listAccounts.filter((x: Account) => x.accountId === account.accountId);
-    if (accountIsExists.length == 1) {
-        const dateRangefilters = getDatesRange();
-        const listTransactions = await readFileDataSource(filePathListTransactions);
-        const today_transactions =
-            listTransactions
-                .filter((x: Transactions) => {
-                    return (new Date(x.transactionDate)).getTime() >= dateRangefilters.Yesterday.getTime() &&
-                        (new Date(x.transactionDate)).getTime() <= dateRangefilters.Tomorrow.getTime();
-                })
-                .map((item: Transactions) => item.value);
-
-        let sum_today_transactions = 0;
-        if (today_transactions.length > 0)
-            sum_today_transactions = today_transactions.reduce((prev: number, next: number) => prev + next);
-
-        const remainDailyDepositValue = account.dailyWithdrawaLimit - sum_today_transactions;
-        if (account.dailyWithdrawaLimit >= sum_today_transactions &&
-            remainDailyDepositValue >= totalDeposit) {
-
-            // update balance account 
-            const indexAccount = listAccounts.findIndex((x: Account) => x.accountId == account.accountId);
-            listAccounts[indexAccount].balance += totalDeposit;
-            account = { ...listAccounts[indexAccount] };
-
-            // add new Transaction
-            const new_transactions: Transactions = {
-                accountId: account,
-                transactionDate: new Date(),
-                transactionId: listTransactions.length + 1,
-                value: totalDeposit
-            };
-            listTransactions.push(new_transactions);
-
-            // update Transaction record
-            const resultWriteFile = await writeFileDataSource(filePathListTransactions, JSON.stringify(listTransactions));
-            if (resultWriteFile === "OK") {
-                const resultWriteFileAccount = await writeFileDataSource(filePathListAccounts, JSON.stringify(listAccounts));
-                if (resultWriteFileAccount === "OK") {
-                    returnMessage.message = `${totalDeposit} value deposit successfully`;
-                    returnMessage.type = true;
-                }
-            }
-        } else {
-            returnMessage.message = `you reached to your dealy limits and you just have this ${remainDailyDepositValue} left`;
-            returnMessage.type = false;
-        }
+    const indexAccount = listAccounts.findIndex((x: Account) => x.accountId == account.accountId);
+    if (indexAccount > 0) {
+        const totalBalance = listAccounts[indexAccount].balance;
+        returnMessage.message = `the balance is : ${totalBalance}`;
+        returnMessage.type = true;
     }
     console.log(returnMessage.message);
     return Promise.resolve(returnMessage);
